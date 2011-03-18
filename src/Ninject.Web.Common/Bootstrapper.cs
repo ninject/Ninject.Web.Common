7888e7adfae6fae95fba62a9c76e888065711a2f
Remo Gloor <remo.gloor@bbv.ch>
2011-03-18 11:17:09 +0100

    Initial Version

---
+++ src/Ninject.Web.Common/Bootstrapper.cs
@@ -0,0 +1,68 @@
+//-------------------------------------------------------------------------------
+// <copyright file="NinjectDependencyResolver.cs" company="bbv Software Services AG">
+//   Copyright (c) 2010 bbv Software Services AG
+//   Author: Remo Gloor (remo.gloor@gmail.com)
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+namespace Ninject.Web.Common
+{
+    using System;
+    using Ninject.Infrastructure.Language;
+
+    /// <summary>
+    /// A basic bootstrapper that can be used to setup web applications.
+    /// </summary>
+    public class Bootstrapper : IBootstrapper
+    {
+        /// <summary>
+        /// The ninject kernel of the application
+        /// </summary>
+        protected static IKernel kernelInstance;
+
+        /// <summary>
+        /// Gets the kernel.
+        /// </summary>
+        public IKernel Kernel
+        {
+            get { return kernelInstance; }
+        }
+
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="createKernelCallback">The create kernel callback function.</param>
+        public void Initialize(Func<IKernel> createKernelCallback)
+        {
+            kernelInstance = createKernelCallback();
+
+            this.Kernel.Components.GetAll<INinjectHttpApplicationPlugin>().Map(c => c.Start());
+            this.Kernel.Inject(this);
+        }
+
+        /// <summary>
+        /// Releases the kernel on application end.
+        /// </summary>
+        public void ShutDown()
+        {
+            if (this.Kernel != null)
+            {
+                this.Kernel.Components.GetAll<INinjectHttpApplicationPlugin>().Map(c => c.Stop());
+                this.Kernel.Dispose();
+                kernelInstance = null;
+            }
+        }
+    }
+}
---
+++ src/Ninject.Web.Common/OnePerRequestModule.cs
@@ -0,0 +1,74 @@
+﻿//-------------------------------------------------------------------------------
+// <copyright file="NinjectHttpApplication.cs" company="bbv Software Services AG">
+//   Copyright (c) 2010 bbv Software Services AG
+//   Author: Remo Gloor (remo.gloor@gmail.com)
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+namespace Ninject.Web.Common
+{
+    using System.Linq;
+    using System.Web;
+
+    using Ninject.Activation.Caching;
+    using Ninject.Infrastructure.Language;
+
+    /// <summary>
+    /// Provides callbacks to more aggressively collect objects scoped to HTTP requests.
+    /// </summary>
+    public class OnePerRequestModule : GlobalKernelRegistry, IHttpModule
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OnePerRequestModule"/> class.
+        /// </summary>
+        public OnePerRequestModule()
+        {
+            this.ReleaseScopeAtRequestEnd = true;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the request scope shall be released immediately after the request has ended.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if the request scope shall be released immediately after the request has ended.; otherwise, <c>false</c>.
+        /// </value>
+        public bool ReleaseScopeAtRequestEnd
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// Initializes the module.
+        /// </summary>
+        /// <param name="application">The <see cref="HttpApplication"/> whose instances will be managed.</param>
+        public void Init(HttpApplication application)
+        {
+            application.EndRequest += (o, e) => this.DeactivateInstancesForCurrentHttpRequest();
+        }
+
+        /// <summary>
+        /// Deactivates instances owned by the current <see cref="HttpContext"/>.
+        /// </summary>
+        public void DeactivateInstancesForCurrentHttpRequest()
+        {
+            if (this.ReleaseScopeAtRequestEnd)
+            {
+                var context = HttpContext.Current;
+                Kernels.Select(kernel => kernel.Components.Get<ICache>()).Map(cache => cache.Clear(context));
+            }
+        }
+    }
+}
