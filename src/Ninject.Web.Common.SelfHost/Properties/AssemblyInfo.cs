6fb66461d91f97e4869c6b4712e0bcc93ebbd537
Marcel Steiner <marcel.steiner@bbv.ch>
2014-03-20 14:13:15 +0100

    Changed build script to run pull request on Teamcity

--- src/Ninject.Web.Common.SelfHost/Properties/AssemblyInfo.cs
+++ src/Ninject.Web.Common.SelfHost/Properties/AssemblyInfo.cs
@@ -24,11 +24,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ninject.Web.Common.SelfHosting")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Ninject.Web.Common.SelfHost")]
+[assembly: AssemblyDescriptionAttribute("Self Host extension for Ninject.Web.Common")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c93e3bee-5dc4-4e95-91ff-dcddc8debd5e")]
